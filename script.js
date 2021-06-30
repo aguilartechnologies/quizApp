704bf6a55d8a5fc5421e02d20a79159435afa51c
createit-ag <85912455+createit-ag@users.noreply.github.com>
2021-06-30 15:15:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-const quizData = [
-    {
-        question: 'How old is Florin?',
-        a: '10',
-        b: '17',
-        c: '26',
-        d: '110',
-        correct: 'c'
-    } {
-        question: 'What is the most used programming language in 2019?',
-        a: 'Java',
-        b: 'C',
-        c: 'Python',
-        d: 'JavaScript',
-        correct: 'd'
-    } {
-        question: 'Who is the president?',
-        a: 'florin pop',
-        b: 'Donald Trump',
-        c: 'Ivan',
-        d: 'Mihai',
-        correct: 'b'
-    } {
-        question: 'What does HTML stand for?',
-        a: 'Hypertext Markup Language',
-        b: 'Cascading Style Sheet',
-        c: 'Jason Object Notation',
-        d: 'Helicopters Terminals Motorboats',
-        correct: 'a'
-    } {
-        question: 'What year was JavaScript launched?',
-        a: '1996',
-        b: '1995',
-        c: '1994',
-        d: 'Non of the above',
-        correct: 'd'
-    }
-]
+const quizData = [ 
+                    { question: 'How old is Florin?',
+                      a: '10',
+                      b: '17',
+                      c: '26',
+                      d: '110',
+                      correct: 'c'
+                    }, 
+                    { question: 'Who is the president?',
+                      a: 'florin pop',
+                      b: 'Donald Trump',
+                      c: 'Ivan',
+                      d: 'Mihai',
+                      correct: 'b'
+                    },
+                    { question: 'What does HTML stand for?',
+                      a: 'Hypertext Markup Language',
+                      b: 'Cascading Style Sheet',
+                      c: 'Jason Object Notation',
+                      d: 'Helicopters Terminals Motorboats',
+                      correct: 'a'
+                    },
+                    { question: 'What year was JavaScript launched?',
+                      a: '1996',
+                      b: '1995',
+                      c: '1994',
+                      d: 'Non of the above',
+                      correct: 'd'
+                    }
+                ]
